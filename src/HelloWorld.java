ab83c14f19315c3ece0c92e721d02826a65bf3c6
kin15016 <kin15016@byui.edu>
2016-09-28 18:31:25 -0700

    Removed 'World'

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -5,6 +5,6 @@ public class HelloWorld {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    System.out.println("Hello GitHub World");
+    System.out.println("Hello GitHub");
   }
 }
